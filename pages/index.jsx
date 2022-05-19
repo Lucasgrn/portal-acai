b0aa8b2f3ef538cccd708ba766d573b629659026
Lucasgrn <dev01@aeoluscloud.com>
2022-05-18 22:43:04 -0300

    Definindo cadastro de clientes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
-import { Box, Text } from '@chakra-ui/react'
-import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Portal do Açaí</title>
-        <meta name="description" content="Portal do Açaí" />
+        <title>Portal do Açaí - Home</title>
+        <meta name="description" content="Home" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
---
+++ pages/register.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>Portal do Açaí - Registro</title>
+        <meta name="description" content="Registro" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+    </>
+  )
+}
