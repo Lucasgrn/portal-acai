97e64393bdcbdb96c323b8d105c95da817388379
Lucasgrn <dev01@aeoluscloud.com>
2022-05-18 22:38:27 -0300

    Navbar pronta

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,13 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Spacer, Text, Image, Link } from "@chakra-ui/react"
 import { HamburgerIcon } from '@chakra-ui/icons'
 export default function Navbar() {
     return (
-        <Box bg='#51a547' w='100%' borderRadius='lg' p='2' boxShadow='base' display='flex'>
-            <Text>Portal do Açaí</Text>
-            <HamburgerIcon/>
+        <Box bg='#51a547' w='100%' borderBottomRadius='lg' p='2%' boxShadow='base' display='flex'>
+            <Link href='/' boxSize='35%' >
+                <Image src='/logoPortal.png' alt='Portal do Açaí' />
+            </Link>
+            <Spacer />
+            <HamburgerIcon boxSize='10%' />
         </Box>
     )
 }
