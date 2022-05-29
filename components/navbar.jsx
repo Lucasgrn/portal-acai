30287a23c6ffcf096244ef4bf573f23b9433777c
Lucasgrn <dev01@aeoluscloud.com>
2022-05-29 20:11:00 -0300

    Corrigindo navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,12 +2,12 @@ import { Box, Spacer, Text, Image, Link } from "@chakra-ui/react"
 import { HamburgerIcon } from '@chakra-ui/icons'
 export default function Navbar() {
     return (
-        <Box bg='#51a547' w='100%' borderBottomRadius='lg' p='2%' boxShadow='base' display='flex'>
-            <Link href='/' boxSize='35%' >
+        <Box bg='#51a547' w='100%' h='70' borderBottomRadius='lg' p='4' boxShadow='base' display='flex'>
+            <Link href='/' boxSize='150' >
                 <Image src='/logoPortal.png' alt='Portal do Açaí' />
             </Link>
             <Spacer />
-            <HamburgerIcon boxSize='10%' />
+            <HamburgerIcon boxSize='30' />
         </Box>
     )
 }
